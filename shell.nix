d3ffaf4a6bbe291f02b8b8a2ed15e13164bbfaae
Emily Lucia Antosch <emilyluciaantosch@web.de>
2025-11-11 13:21:44 +0100

    Add(gitignore): Added and merges gitignore

--- shell.nix
+++ shell.nix
@@ -1,17 +1,47 @@
 {
-  pkgs ? import <nixpkgs-unstable> { },
+  pkgs ? import <nixpkgs-unstable> { }
 }:
-pkgs.mkShell {
+pkgs.mkShell rec {
   nativeBuildInputs = with pkgs; [
     rustc
     rustfmt
     clippy
     cargo
+    wayland-utils
     openssl
+    glfw-wayland
     xdg-utils
     sqlite
     sqlite.dev
     openssl.dev
     pkg-config
+    waylandpp.dev
+    wlvncc
+    libxkbcommon.dev
+    xkbd
+    xwayland
+  ];
+  buildInputs = with pkgs; [
+    at-spi2-atk
+    libxkbcommon.dev
+    alsa-lib
+    libudev-zero
+    atkmm
+    cairo
+    gdk-pixbuf
+    glib
+    gtk3
+    harfbuzz
+    librsvg
+    vulkan-loader
+    vulkan-tools
+    libsoup_3
+    pango
+    webkitgtk_4_1
+    openssl
+  ];
+  LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+    pkgs.vulkan-loader
+    pkgs.libxkbcommon
   ];
 }
